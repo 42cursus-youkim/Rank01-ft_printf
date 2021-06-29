972354cebdfda8291dd30dfde2d4c71e1379188d
youkim <42.4.youkim@gmail.com>
2021-06-29 16:27:52 +0900

    feat: :sparkles: support hax (still on reverse)

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: youkim <youkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 15:12:45 by youkim            #+#    #+#             */
-/*   Updated: 2021/06/29 15:59:03 by youkim           ###   ########.fr       */
+/*   Updated: 2021/06/29 16:15:13 by youkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,12 @@
 typedef	struct	s_info
 {
 	char		type;
+	int			num_base;
 	// int			width;
 	// int			prec;
 	// int			zeropad;
 	// int			is_zero;
 	// int			dosign;
-	// int			nbr_base;
 	// int			nbr_sign;
 }				t_info;
 
@@ -51,11 +51,11 @@ int				ft_printf(const char *format, ...);
 int				print_char(int c, t_info *info);
 int				print_string(char *s, t_info *info);
 int				print_number(unsigned long long n, t_info *info);
+
 /*
 ** UTILS
 */
 void			init_info(t_info *info);
-int				ft_numlen(unsigned long long n, t_info *info);
 int				print_type(va_list ap, t_info *info);
 void			check_info(va_list ap, char *format, t_info *info, int i);
 
--- print_number.c
+++ print_number.c
@@ -6,18 +6,58 @@
 /*   By: youkim <youkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 15:01:29 by youkim            #+#    #+#             */
-/*   Updated: 2021/06/29 16:04:47 by youkim           ###   ########.fr       */
+/*   Updated: 2021/06/29 16:26:38 by youkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int		numlen(unsigned long long n, t_info *info)
+{
+	size_t i;
+	// TODO: chek prec
+	i = 0;
+	while (n)
+	{
+		i++;
+		n /= 10; // FIXME: it should implement info->num_base
+	}
+
+	return (i);
+}
+
+#include <stdio.h>
+
+int putdigit(unsigned long long n, t_info *info)
+{
+	char *base;
+
+	if (ft_strchr("px",info->type))
+		base = "0123456789abcdef";
+	else if (info->type == 'X')
+		base = "0123456789ABCDEF";
+	else
+		base = "0123456789";
+	ft_putchar(base[n % info->num_base]);
+}
+
 int	print_number(unsigned long long n, t_info *info)
 {
-	int result;
+	int		result;
+	// char	*buffer;
 
-	result = 0;
+	if (ft_strchr("pxX", info->type))
+		info->num_base = 16;
+
 	// TODO: string parsing
-	ft_putnbr_fd(n, 1); // TODO: need to handle number length
+	result = numlen(n, info);
+	// printf("<RES: %d>", result);
+	while (n) // FIXME: it shows reverse
+	{
+		putdigit(n, info);
+		n /= info->num_base;
+	}
+	// ft_putnbr_fd(n, 1); // TODO: need to handle number length
+	// ft_putstr(buffer);
 	return (result);
 }

a9282f2da6835d069213f77577ce3a8b04e59c9d
youkim <42.4.youkim@gmail.com>
2021-06-29 16:05:07 +0900

    feat: :poop: it does *print* numbers, you see

--- print_number.c
+++ print_number.c
@@ -6,7 +6,7 @@
 /*   By: youkim <youkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 15:01:29 by youkim            #+#    #+#             */
-/*   Updated: 2021/06/29 15:42:53 by youkim           ###   ########.fr       */
+/*   Updated: 2021/06/29 16:04:47 by youkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ int	print_number(unsigned long long n, t_info *info)
 	int result;
 
 	result = 0;
-	ft_putstr("FIXME"); // FIXME: actually print numbers
 	// TODO: string parsing
-	// result += ft_putstr(n);
+	ft_putnbr_fd(n, 1); // TODO: need to handle number length
 	return (result);
 }

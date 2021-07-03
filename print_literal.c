327aebce1b7e78788effd90da4732ab6d44bf963
youkim <42.4.youkim@gmail.com>
2021-07-03 15:15:23 +0900

    chore: :truck: merged print_char & print_string (they had only one funcs each anyway)

--- print_literal.c
+++ print_literal.c
@@ -6,11 +6,25 @@
 /*   By: youkim <youkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 15:01:29 by youkim            #+#    #+#             */
-/*   Updated: 2021/07/03 15:08:46 by youkim           ###   ########.fr       */
+/*   Updated: 2021/07/03 15:14:00 by youkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	print_char(int c, t_info *info)
+{
+	int		result;
+
+	result = 0;
+	if (info->type == '%' && info->lalign)
+		info->zeropad = false;
+	if (info->lalign)
+		result += ft_putchar(c) + pad(info, 1);
+	else
+		result += pad(info, 1) + ft_putchar(c);
+	return (result);
+}
 
 int	print_string(char *s, t_info *info)
 {

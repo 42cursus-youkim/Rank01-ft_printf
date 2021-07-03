069d18e00cdf8924cb7f3fa085357bc25f8fc9de
youkim <42.4.youkim@gmail.com>
2021-07-03 15:08:01 +0900

    feat: :art: print char & str pad merge preparations

--- print_char.c
+++ print_char.c
@@ -6,18 +6,18 @@
 /*   By: youkim <youkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 14:44:38 by youkim            #+#    #+#             */
-/*   Updated: 2021/07/03 14:28:47 by youkim           ###   ########.fr       */
+/*   Updated: 2021/07/03 15:05:52 by youkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static	int	pad(t_info *info)
+static	int	pad(t_info *info, int len)
 {
 	int	result;
 
 	result = 0;
-	while (--info->width > 0)
+	while (len++ < info->width)
 	{
 		if (info->zeropad)
 			ft_putchar('0');
@@ -36,8 +36,8 @@ int	print_char(int c, t_info *info)
 	if (info->type == '%' && info->lalign)
 		info->zeropad = false;
 	if (info->lalign)
-		result += ft_putchar(c) + pad(info);
+		result += ft_putchar(c) + pad(info, 1);
 	else
-		result += pad(info) + ft_putchar(c);
+		result += pad(info, 1) + ft_putchar(c);
 	return (result);
 }

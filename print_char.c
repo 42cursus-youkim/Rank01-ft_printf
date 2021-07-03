0873fdb2e5ea5e151d39edd04b639c0c366b364c
youkim <42.4.youkim@gmail.com>
2021-07-03 13:03:30 +0900

    fix: :bug: function definition moved to up

--- print_char.c
+++ print_char.c
@@ -6,11 +6,27 @@
 /*   By: youkim <youkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 14:44:38 by youkim            #+#    #+#             */
-/*   Updated: 2021/07/03 12:54:43 by youkim           ###   ########.fr       */
+/*   Updated: 2021/07/03 13:01:33 by youkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	print_width(t_info *info)
+{
+	int	result;
+
+	result = 0;
+	while (info->width--)
+	{
+		if (info->zeropad)
+			ft_putchar('0');
+		else
+			ft_putchar(' ');
+		result++;
+	}
+	return (result);
+}
 
 int	print_char(int c, t_info *info)
 {
@@ -18,27 +34,10 @@ int	print_char(int c, t_info *info)
 
 	result = 0;
 	if (info->type == '%' && info->lalign)
-		info->zeropad = true;
+		info->zeropad = false;
 	if (info->lalign)
 		result += ft_putchar(c) + print_width(info);
 	else
 		result += print_width(info) + ft_putchar(c);
-	return (result);
-}
-
-static int	print_width(t_info *info)
-{
-	int	result;
-
-	result = 0;
-	while (info->width)
-	{
-		if (info->zeropad)
-			ft_putchar('0');
-		else
-			ft_putchar(' ');
-		info->width--;
-		result++;
-	}
 	return (result);
 }

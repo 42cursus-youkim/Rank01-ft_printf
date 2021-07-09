a96f78c34730c442ac53a26414c902c0648df1a3
youkim <42.4.youkim@gmail.com>
2021-07-09 17:00:34 +0900

    fix: :bug: fix for negative precision

--- print_number.c
+++ print_number.c
@@ -6,23 +6,24 @@
 /*   By: youkim <youkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 15:01:29 by youkim            #+#    #+#             */
-/*   Updated: 2021/07/09 16:32:24 by youkim           ###   ########.fr       */
+/*   Updated: 2021/07/09 17:00:04 by youkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-static	int	pad_width(t_info *info, bool is_noprec)
+static	int	pad_width(t_info *info, int temprec)
 {
 	int	i;
 	int	result;
 
 	i = info->prec;
 	result = 0;
+
 	while (info->num_minus + i++ < info->width)
 	{
-		if (info->zeropad && !info->lalign && is_noprec)
+		if (info->zeropad && !info->lalign && temprec < 0)
 			result += ft_putchar('0');
 		else
 			result += ft_putchar(' ');
@@ -34,22 +35,22 @@ static	int	pad(char *s, t_info *info)
 {
 	int		start;
 	int		result;
-	bool	is_noprec;
+	int		temprec;
 	result = 0;
 	start = ft_strlen(s);
-	is_noprec = (info->prec == NOPREC);
+	temprec = info->prec;
 	info->prec = ft_max(info->prec, ft_strlen(s));
-	if (info->num_minus && (info->zeropad && is_noprec))
+	if (info->num_minus && (info->zeropad && temprec))
 		result += ft_putchar('-');
 	if (!info->lalign)
-		result += pad_width(info, is_noprec);
-	if (info->num_minus && !(info->zeropad && is_noprec))
+		result += pad_width(info, temprec);
+	if (info->num_minus && !(info->zeropad && temprec))
 		result += ft_putchar('-');
 	while (start++ < info->prec)
 		result += ft_putchar('0');
 	result += ft_putstr(s);
 	if (info->lalign)
-		result += pad_width(info, is_noprec);
+		result += pad_width(info, temprec);
 	return (result);
 }
 
